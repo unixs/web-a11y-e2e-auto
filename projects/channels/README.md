03e9471f44b409aa8cb6e6fa64a44b9a4b1f9ae0
Alexander Feodorov <vester@unixcomp.org>
2023-11-27 22:22:20 +0400

    structure

--- projects/channels/README.md
+++ projects/channels/README.md
@@ -1,4 +1,4 @@
 # web-a11y-e2e-auto
 Investigation of e2e tests for web a11y
 
-## Root module
+## Channels QA module
---
+++ projects/channels/src/main/java/net/unixcode/qa/auto/projects/channels/Main.java
@@ -0,0 +1,31 @@
+package net.unixcode.qa.auto.projects.channels;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+
+public class Main {
+  final static protected Logger log = LoggerFactory.getLogger(Main.class);
+  public static void main(String @NotNull [] argv) {
+    try {
+      ApplicationContext ctxt = new AnnotationConfigApplicationContext(DIConfig.class);
+
+      log.info("Ok");
+    }
+    catch (Exception e) {
+      log.error(e.getMessage());
+      System.exit(1);
+    }
+  }
+
+  @Configuration
+  @Lazy
+  @ImportResource(locations = {"classpath:spring/context.xml"})
+  @ComponentScan(basePackages = { "net.unixcode.qa.auto.projects.channels" })
+  @PropertySource("classpath:application.properties")
+  static class DIConfig {
+  }
+}
